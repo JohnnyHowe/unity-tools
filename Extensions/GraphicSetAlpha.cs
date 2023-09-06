ccef15ab386ef289e7995a5b8a12d504a00d02e1
Johnny Howe <jonathon.o.howe@gmail.com>
2023-09-06 14:02:59 +1200

    Graphic > MaskableGraphic

--- Extensions/GraphicSetAlpha.cs
+++ Extensions/GraphicSetAlpha.cs
@@ -3,9 +3,9 @@ using UnityEngine.UI;
 
 namespace JonathonOH.UnityExtensions
 {
-    public static class ImageSetAlpha
+    public static class GraphicSetAlpha
     {
-        public static void SetAlpha(this MaskableGraphic graphic, float newAlpha)
+        public static void SetAlpha(this Graphic graphic, float newAlpha)
         {
             Color c = graphic.color;
             c.a = newAlpha;
